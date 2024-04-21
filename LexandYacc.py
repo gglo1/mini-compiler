d4309efce9f3c70ca3798694cf44171a6fa27b3a
Khafaniking <80512615+Khafaniking@users.noreply.github.com>
2024-04-21 02:06:34 -0800

    Update LexandYacc.py
    Added reserved words for future progress towards control statements. Added eq, neq, lt, and gt. Added token def for ID

--- LexandYacc.py
+++ LexandYacc.py
@@ -3,7 +3,15 @@ import ply.yacc as yacc
 import math
 
 #our tokens
-tokens = ( 'NUMBER', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN', 'POWER', 'SIN', 'COS', 'TAN', 'LN', 'LOG', 'EXP')
+
+reserved = {
+   'if' : 'IF',
+   'then' : 'THEN',
+   'else' : 'ELSE',
+   'while' : 'WHILE',
+}
+tokens = ( 'NUMBER', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN', 'POWER', 'SIN', 'COS', 'TAN', 'LN', 'LOG', 'EXP',
+          'EQ', 'NEQ', 'LT', 'GT', 'ID',) + tuple(reserved.values())
 
 # Regular expression rules for simple tokens
 t_PLUS = r'\+'
@@ -14,15 +22,26 @@ t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_POWER = r'\^'
 
+t_EQ = r'=='
+t_NEQ = r'!='
+t_LT = r'<'
+t_GT = r'>'
+
 t_SIN = r'sin'
 t_COS = r'cos'
 t_TAN = r'tan'
 t_LN = r'ln'
 t_LOG = r'log'
 t_EXP = r'exp'
-#our rules for our tokens
 
+
+#our rules for our tokens
 #creation of a token
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID')    # Check for reserved words
+    return t
+    
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
@@ -63,7 +82,7 @@ precedence = (
         #break      # No more input
     #print(tok)
 
-#2. handle semantic analysis
+#2. handle syntax analysis
 
 def p_expression_plus(p):
     'expression : expression PLUS term'
@@ -100,6 +119,22 @@ def p_factor_expr(p):
 def p_factor_power(p):
     'factor : factor POWER factor'
     p[0] = p[1] ** p[3]
+
+def p_expression_equal(p):
+    'expression : expression EQ expression'
+    p[0] = p[1] == p[3]
+
+def p_expression_not_equal(p):
+    'expression : expression NEQ expression'
+    p[0] = p[1] != p[3]
+
+def p_expression_less_than(p):
+    'expression : expression LT expression'
+    p[0] = p[1] < p[3]
+
+def p_expression_greater_than(p):
+    'expression : expression GT expression'
+    p[0] = p[1] > p[3]
 
 def p_factor_sin(p):
     'factor : SIN LPAREN expression RPAREN'
